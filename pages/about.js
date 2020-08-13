682007c55b5ae4aee8ebc9cd41842ebbb17e5172
Jacky Rusly <jacky.rusly@lemonilo.com>
2020-08-13 16:24:41 +0700

    Add some styles

--- pages/about.js
+++ pages/about.js
@@ -7,6 +7,9 @@ const About = () => {
   return (
     <View style={[t.itemsCenter, t.justifyCenter]}>
       <Text>About React Native</Text>
+      <Text style={t.textGreen500}>Text green</Text>
+      <Text style={t.textYellow300}>Text yellow</Text>
+      <Text style={t.textPurple100}>Text purple</Text>
       <Link href="/">
           <a>Home</a>
       </Link>
